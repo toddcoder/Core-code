4796a8ce6e09c10061db66e851c489975a493e73
Todd Bennett <toddcoder@comcast.net>
2019-08-23 08:30:40 -0500

    suffix must be a space for --boolean to work

--- Applications/CommandLineInterface.cs
+++ Applications/CommandLineInterface.cs
@@ -95,7 +95,7 @@ namespace Core.Applications
                var rest = matcher.FirstGroup;
                if (type == typeof(bool))
                {
-                  if (!rest.IsMatch("^ /s* 'false' | 'true'"))
+                  if (suffix == " " && !rest.IsMatch("^ /s* 'false' | 'true'"))
                   {
                      return true.Success<object>();
                   }
