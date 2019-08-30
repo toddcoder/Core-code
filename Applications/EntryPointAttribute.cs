2e8fb53f46ecb7e1e0f72cb83e7e2d7ae6ae7e0d
Todd Bennett <tebennett@eprod.com>
2019-08-30 08:57:51 -0500

    Added UsesObject property

--- Applications/EntryPointAttribute.cs
+++ Applications/EntryPointAttribute.cs
@@ -2,5 +2,13 @@
 
 namespace Core.Applications
 {
-   public class EntryPointAttribute : Attribute { }
+   public class EntryPointAttribute : Attribute
+   {
+      public EntryPointAttribute(bool usesObject = false)
+      {
+         UsesObject = usesObject;
+      }
+
+      public bool UsesObject { get; }
+   }
 }
