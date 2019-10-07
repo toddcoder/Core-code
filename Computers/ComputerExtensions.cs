9d9eed09d8a53993a7a31a473f08cd4ab8afea5f
Todd Bennett <tebennett@eprod.com>
2019-10-07 14:48:59 -0500

    Added AsFileName and AsFolderName extensions functions for strings

---
+++ Computers/ComputerExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Core.Monads;
+
+namespace Core.Computers
+{
+   public static class ComputerExtensions
+   {
+      public static IMaybe<FileName> AsFileName(this string file) => ((FileName)file).Some();
+
+      public static IMaybe<FolderName> AsFolderName(this string folder) => ((FolderName)folder).Some();
+   }
+}
