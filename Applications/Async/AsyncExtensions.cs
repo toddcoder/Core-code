173da87e8564e2e23fe7ce800060a3c601170898
Todd Bennett <tebennett@eprod.com>
2019-11-08 09:37:06 -0600

    Added AsyncExtensions

---
+++ Applications/Async/AsyncExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Core.Monads;
+
+namespace Core.Applications.Async
+{
+   public static class AsyncExtensions
+   {
+      public static async Task<ICompletion<TResult>> Map<T, TResult>(this Task<ICompletion<T>> task, Func<T, TResult> func)
+      {
+         return await Task.Run(() => task.Result.Map(func));
+      }
+
+      public static async Task<ICompletion<TResult>> Map<T, TResult>(this Task<ICompletion<T>> task, Func<T, ICompletion<TResult>> func)
+      {
+         return await Task.Run(() => task.Result.Map(func));
+      }
+   }
+}
