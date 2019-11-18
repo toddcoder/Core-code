14fca85397a5d8d42473fe2c37a22bcafd12d3fd
Todd Bennett <tebennett@eprod.com>
2019-11-18 08:37:56 -0600

    ElapsedTime class

---
+++ Dates/ElapsedTime.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Core.Dates
+{
+   public class ElapsedTime
+   {
+      public static implicit operator TimeSpan(ElapsedTime elapsedTime) => elapsedTime.Elapsed;
+
+      public static implicit operator string(ElapsedTime elapsedTime) => elapsedTime.ToString();
+
+      bool includeMilliseconds;
+      Stopwatch stopwatch;
+
+      public ElapsedTime(bool includeMilliseconds = true)
+      {
+         this.includeMilliseconds = includeMilliseconds;
+
+         stopwatch = new Stopwatch();
+         stopwatch.Start();
+      }
+
+      public TimeSpan Elapsed
+      {
+         get
+         {
+            stopwatch.Stop();
+            return stopwatch.Elapsed;
+         }
+      }
+
+      public override string ToString() => Elapsed.ToLongString(includeMilliseconds);
+   }
+}
