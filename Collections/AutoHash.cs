e6875b8d6dec81456734814718aa0a3980bcd834
Todd Bennett <tebennett@eprod.com>
2019-12-18 12:23:28 -0600

    AutoHash now has AddKeys method

--- Collections/AutoHash.cs
+++ Collections/AutoHash.cs
@@ -1,36 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.Serialization;
+using Core.Assertions;
 
 namespace Core.Collections
 {
    public class AutoHash<TKey, TValue> : Hash<TKey, TValue>
    {
-      public AutoHash() { }
+      Func<TKey, TValue> defaultLambda;
 
-      public AutoHash(int capacity) : base(capacity) { }
+      public AutoHash()
+      {
+         defaultLambda = key => default;
+      }
 
-      public AutoHash(IEqualityComparer<TKey> comparer) : base(comparer) { }
+      public AutoHash(int capacity) : base(capacity)
+      {
+         defaultLambda = key => default;
+      }
 
-      public AutoHash(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer) { }
+      public AutoHash(IEqualityComparer<TKey> comparer) : base(comparer)
+      {
+         defaultLambda = key => default;
+      }
 
-      public AutoHash(IDictionary<TKey, TValue> dictionary) : base(dictionary) { }
+      public AutoHash(int capacity, IEqualityComparer<TKey> comparer) : base(capacity, comparer)
+      {
+         defaultLambda = key => default;
+      }
 
-      public AutoHash(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) : base(dictionary, comparer) { }
+      public AutoHash(IDictionary<TKey, TValue> dictionary) : base(dictionary)
+      {
+         defaultLambda = key => default;
+      }
 
-      protected AutoHash(SerializationInfo info, StreamingContext context) : base(info, context) { }
+      public AutoHash(IDictionary<TKey, TValue> dictionary, IEqualityComparer<TKey> comparer) : base(dictionary, comparer)
+      {
+         defaultLambda = key => default;
+      }
+
+      protected AutoHash(SerializationInfo info, StreamingContext context) : base(info, context)
+      {
+         defaultLambda = key => default;
+      }
 
       public AutoHash(Func<TKey, TValue> defaultLambda, bool autoAddDefault = false)
       {
+         defaultLambda.Must().Not.BeNull().Assert();
+
          Default = DefaultType.Lambda;
-         DefaultLambda = defaultLambda;
+         this.defaultLambda = defaultLambda;
          AutoAddDefault = autoAddDefault;
       }
 
       public AutoHash(Func<TKey, TValue> defaultLambda, bool autoAddDefault, IEqualityComparer<TKey> comparer) : this(comparer)
       {
+         defaultLambda.Must().Not.BeNull().Assert();
+         comparer.Must().Not.BeNull().Assert();
+
          Default = DefaultType.Lambda;
-         DefaultLambda = defaultLambda;
+         this.defaultLambda = defaultLambda;
          AutoAddDefault = autoAddDefault;
       }
 
@@ -39,20 +69,34 @@ namespace Core.Collections
          Default = DefaultType.Value;
          DefaultValue = defaultValue;
          AutoAddDefault = autoAddDefault;
+
+         defaultLambda = key => default;
       }
 
       public AutoHash(TValue defaultValue, bool autoAddDefault, IEqualityComparer<TKey> comparer) : this(comparer)
       {
+         comparer.Must().Not.BeNull().Assert();
+
          Default = DefaultType.Value;
          DefaultValue = defaultValue;
          AutoAddDefault = autoAddDefault;
+
+         defaultLambda = key => default;
       }
 
       public DefaultType Default { get; set; } = DefaultType.Value;
 
       public TValue DefaultValue { get; set; }
 
-      public Func<TKey, TValue> DefaultLambda { get; set; } = key => default;
+      public Func<TKey, TValue> DefaultLambda
+      {
+         get => defaultLambda;
+         set
+         {
+            value.Must().Not.BeNull().Assert();
+            defaultLambda = value;
+         }
+      }
 
       public bool AutoAddDefault { get; set; }
 
@@ -60,6 +104,8 @@ namespace Core.Collections
       {
          get
          {
+            key.Must().Not.BeNull().Assert();
+
             TValue result;
             switch (Default)
             {
@@ -88,11 +134,6 @@ namespace Core.Collections
                   }
                   else
                   {
-                     if (DefaultLambda == null)
-                     {
-                        DefaultLambda = k => default;
-                     }
-
                      result = DefaultLambda(key);
                      if (AutoAddDefault)
                      {
@@ -107,6 +148,30 @@ namespace Core.Collections
             }
          }
          set => base[key] = value;
+      }
+
+      public void AddKeys(IEnumerable<TKey> keys)
+      {
+         Func<TKey, TValue> valueFunc;
+         switch (Default)
+         {
+            case DefaultType.None:
+               valueFunc = k => default;
+               break;
+            case DefaultType.Value:
+               valueFunc = k => DefaultValue;
+               break;
+            case DefaultType.Lambda:
+               valueFunc = DefaultLambda;
+               break;
+            default:
+               return;
+         }
+
+         foreach (var key in keys.Where(k => k != null && !ContainsKey(k)))
+         {
+            this[key] = valueFunc(key);
+         }
       }
    }
 }
---
+++ Core.Tests/HashTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Core.Collections;
+using Core.Strings;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Core.Arrays.ArrayFunctions;
+
+namespace Core.Tests
+{
+   [TestClass]
+   public class HashTest
+   {
+      static void test(AutoHash<string, int> autoHash, string message, Func<string, int> newLambda)
+      {
+         var keys = array("alpha", "bravo", "charlie");
+
+         Console.WriteLine("-".Repeat(80));
+         Console.WriteLine(message.LeftJustify(80, '-'));
+         foreach (var key in keys)
+         {
+            Console.WriteLine($"{key}: {autoHash[key]}");
+         }
+
+         autoHash.DefaultLambda = newLambda;
+
+         Console.WriteLine("-".Repeat(80));
+         foreach (var key in keys)
+         {
+            Console.WriteLine($"{key}: {autoHash[key]}");
+         }
+      }
+
+      [TestMethod]
+      public void AutoHashTest()
+      {
+         test(new AutoHash<string, int>(k => -1), "No auto-add", k => -2);
+         test(new AutoHash<string, int>(k => -1, true), "Auto-add", k => -2);
+
+         var autoHash = new AutoHash<string, int>(k => k.Length);
+         autoHash.AddKeys(new List<string> { "alpha", "bravo" });
+         test(autoHash, "AddKeys", k => -k.Length);
+      }
+   }
+}
