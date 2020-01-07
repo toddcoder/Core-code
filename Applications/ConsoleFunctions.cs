8b0d524b9321c86da65d176d8f1ed04cbdaadba2
Todd Bennett <tebennett@eprod.com>
2020-01-07 12:25:10 -0600

    Progress functions for console

--- Applications/ConsoleFunctions.cs
+++ Applications/ConsoleFunctions.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Runtime.InteropServices;
 using Core.Monads;
 
@@ -7,8 +8,11 @@ namespace Core.Applications
 	public static class ConsoleFunctions
 	{
 		const uint ERROR_ACCESS_DENIED = 5;
+      const char BLOCK = '■';
+      const string BACK = "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b";
+      const string TWIRL = "-\\|/";
 
-		static string errorMessage() => new Win32Exception(Marshal.GetLastWin32Error()).Message;
+      static string errorMessage() => new Win32Exception(Marshal.GetLastWin32Error()).Message;
 
 		public static IResult<Unit> consoleNew(bool alwaysCreateNewConsole = true)
 		{
@@ -23,6 +27,42 @@ namespace Core.Applications
          {
             return errorMessage().Failure<Unit>();
          }
+      }
+
+      public static void writeProgressBar(int percent, bool update = false)
+      {
+         if (update)
+         {
+            Console.Write(BACK);
+         }
+
+         Console.Write("[");
+
+         var percentAsInt = (int)(percent / 10f + 0.5f);
+
+         for (var i = 0; i < 10; i++)
+         {
+            if (i >= percentAsInt)
+            {
+               Console.Write(' ');
+            }
+            else
+            {
+               Console.Write(BLOCK);
+            }
+         }
+
+         Console.Write($"] {percent:##0}");
+      }
+
+      public static void writeProgress(int progress, bool update = false)
+      {
+         if (update)
+         {
+            Console.Write("\b");
+         }
+
+         Console.Write(TWIRL[progress % TWIRL.Length]);
       }
 	}
 }
