546d1d524da428457023379b005638a82629d093
Todd Bennett <tebennett@eprod.com>
2020-01-09 08:18:56 -0600

    Updated version to 1.0.5.0

--- Core.Tests/Properties/AssemblyInfo.cs
+++ Core.Tests/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Core.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyCopyright("Copyright © 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
