d3dc655ba9143a8399ac107366dc11a0bfdff947
Todd Bennett <tebennett@eprod.com>
2020-04-15 09:41:17 -0500

    Extensions to split enumerables of tuples

---
+++ Core.Tests/TupleTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Core.Assertions;
+using Core.Enumerables;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Core.Arrays.ArrayFunctions;
+using static Core.Assertions.AssertionFunctions;
+
+namespace Core.Tests
+{
+   [TestClass]
+   public class TupleTests
+   {
+      [TestMethod]
+      public void SplitTest()
+      {
+         var enumerable = array(("alpha", 1), ("bravo", 2), ("charlie", 3));
+         var (enumerable1, enumerable2) = enumerable.Split();
+         var text1 = enumerable1.Stringify();
+         var text2 = enumerable2.Stringify();
+
+         assert(() => text1).Must().Equal("alpha, bravo, charlie").OrThrow();
+         Console.WriteLine($"First enumerable = {text1}");
+
+         assert(() => text2).Must().Equal("1, 2, 3").OrThrow();
+         Console.WriteLine($"Second enumerable = {text2}");
+      }
+   }
+}
