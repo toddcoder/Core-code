3f722e423b4282f2873da5560a80ddde87e1a6a2
Todd Bennett <tebennett@eprod.com>
2020-07-06 09:39:58 -0500

    Added assertion for resources

--- Applications/Resources.cs
+++ Applications/Resources.cs
@@ -1,32 +1,41 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using Core.Assertions;
+using static Core.Assertions.AssertionFunctions;
 
 namespace Core.Applications
 {
-	public class Resources<T>
-	{
-		protected Type type;
-		protected string nameSpace;
-		protected string[] names;
+   public class Resources<T>
+   {
+      protected Type type;
+      protected string nameSpace;
+      protected string[] names;
 
-		public Resources()
-		{
-			type = typeof(T);
-			nameSpace = type.Namespace + ".";
-			names = type.Assembly.GetManifestResourceNames();
-		}
+      public Resources()
+      {
+         type = typeof(T);
+         nameSpace = type.Namespace + ".";
+         names = type.Assembly.GetManifestResourceNames();
+      }
 
-		public string String(string name)
-		{
-			using (var reader = new StreamReader(Stream(name)))
+      public string String(string name)
+      {
+         assert(() => Contains(name)).Must().BeTrue().OrThrow($"Resource {nameSpace + name} does not exist");
+         using (var reader = new StreamReader(Stream(name)))
          {
             return reader.ReadToEnd();
          }
       }
 
-		public Stream Stream(string name) => type.Assembly.GetManifestResourceStream(nameSpace + name);
+      public Stream Stream(string name)
+      {
+         var fullName = nameSpace + name;
+         assert(() => Contains(name)).Must().BeTrue().OrThrow($"Resource {fullName} does not exist");
 
-		public bool Contains(string name) => names.Contains(nameSpace + name);
-	}
+         return type.Assembly.GetManifestResourceStream(fullName);
+      }
+
+      public bool Contains(string name) => names.Contains(nameSpace + name);
+   }
 }
