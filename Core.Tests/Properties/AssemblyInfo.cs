7089d67ec02df09da27c539f1df01120832eeda7
Todd Bennett <tebennett@eprod.com>
2020-08-13 13:34:04 -0500

    Fixed Adapter.TryTo.Execute

--- Core.Tests/Properties/AssemblyInfo.cs
+++ Core.Tests/Properties/AssemblyInfo.cs
@@ -15,5 +15,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("77228c9d-168e-4959-9a5c-361625803672")]
 
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.3.3")]
-[assembly: AssemblyFileVersion("1.1.3.3")]
+[assembly: AssemblyVersion("1.1.3.4")]
+[assembly: AssemblyFileVersion("1.1.3.4")]
