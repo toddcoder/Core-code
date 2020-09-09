0e6b48da8ac5046a3e7929faa3575d46eeb5bf41
Todd Bennett <tebennett@eprod.com>
2020-09-09 08:15:43 -0500

    EqSet

---
+++ Collections/EqSet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core.Collections
+{
+   public class EqSet<T> : Set<T>
+   {
+      protected Func<T, T, bool> equals;
+
+      public EqSet(Func<T, T, bool> equals)
+      {
+         this.equals = equals;
+      }
+
+      public EqSet(IEnumerable<T> items, Func<T, T, bool> equals) : base(items)
+      {
+         this.equals = equals;
+      }
+
+      public EqSet(Set<T> other, Func<T, T, bool> equals) : base(other)
+      {
+         this.equals = equals;
+      }
+
+      public override bool Contains(T item)
+      {
+         for (var i = 0; i < Count; i++)
+         {
+            if (equals(content[i], item))
+            {
+               return true;
+            }
+         }
+
+         return false;
+      }
+   }
+}
