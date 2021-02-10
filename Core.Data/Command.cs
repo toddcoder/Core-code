f51effcbb4f11b62202601e54237aba5ec681c2d
Todd Bennett <toddcoder@comcast.net>
2021-02-10 14:01:09 -0600

    Revert "ff"
    This reverts commit bd47cfbef0b24c4abc760c1e89a9ef9afeb164b8.

--- Core.Data/Command.cs
+++ Core.Data/Command.cs
@@ -1,15 +1,80 @@
 ﻿using System;
+using Core.Collections;
+using Core.Computers;
+using Core.Dates;
 using Core.Dates.DateIncrements;
+using Core.Monads;
+using Core.ObjectGraphs;
 
 namespace Core.Data
 {
    public class Command
    {
+      public static IResult<Command> FromObjectGraph(ObjectGraph commandGraph)
+      {
+         var name = commandGraph.Name;
+         return
+            from values in getValues(commandGraph)
+            select new Command { Name = name, Text = values.text, CommandTimeout = values.timeout };
+      }
+
+      static IResult<(string text, TimeSpan timeout)> getValues(ObjectGraph commandGraph)
+      {
+         if (commandGraph.HasChildren)
+         {
+            if (commandGraph.ChildExists("command"))
+            {
+               return
+                  from timeout in commandGraph.FlatMap("timeout", g => g.Value.TimeSpan(), () => 30.Seconds().Success())
+                  from textGraph in commandGraph.Require("text")
+                  from text in textGraph.Value.Success()
+                  select (text, timeout);
+            }
+
+            if (commandGraph.ChildExists("file"))
+            {
+               return
+                  from timeout in commandGraph.FlatMap("timeout", g => g.Value.TimeSpan(), () => 30.Seconds().Success())
+                  from fileGraph in commandGraph.Require("file")
+                  from file in FileName.Try.FromString(fileGraph.Value)
+                  from text in file.TryTo.Text
+                  select (text, timeout);
+            }
+         }
+
+         return (commandGraph.Value, 30.Seconds()).Success();
+      }
+
+      public Command(ObjectGraph commandGraph)
+      {
+         Name = commandGraph.Name;
+         if (commandGraph.HasChildren)
+         {
+            CommandTimeout = commandGraph.FlatMap("timeout", g => g.Value.ToTimeSpan(), () => 30.Seconds());
+            if (commandGraph.If("command", out var text))
+            {
+               Text = text.Value;
+            }
+            else
+            {
+               FileName fileName = commandGraph["file"].Value;
+               Text = fileName.Text;
+            }
+         }
+         else
+         {
+            Text = commandGraph.Value;
+            CommandTimeout = 30.Seconds();
+         }
+
+         Text = commandGraph.Replace(Text);
+      }
+
       internal Command()
       {
-         Name = string.Empty;
+         Name = "";
          CommandTimeout = 30.Seconds();
-         Text = string.Empty;
+         Text = "";
       }
 
       public string Name { get; set; }
