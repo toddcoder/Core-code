996cfe89ac9482542af2f44d40e8f13ed5b6f24a
Todd Bennett <tebennett@eprod.com>
2021-04-06 12:54:52 -0500

    Fixed bug in StringSet.Remove when igncase is true

--- Collections/StringSet.cs
+++ Collections/StringSet.cs
@@ -4,27 +4,58 @@ using System.Linq;
 using Core.Enumerables;
 using Core.Monads;
 using Core.Strings;
+using static Core.Monads.MonadFunctions;
 
 namespace Core.Collections
 {
    public class StringSet : Set<string>
    {
-      static StringComparison getStringComparison(bool ignoreCase)
+      protected static StringComparison getStringComparison(bool ignoreCase)
       {
          return ignoreCase ? StringComparison.OrdinalIgnoreCase : StringComparison.Ordinal;
       }
 
+      protected static Func<string, List<string>, IMaybe<int>> getFinder(bool ignoreCase)
+      {
+         return ignoreCase ? (Func<string, List<string>, IMaybe<int>>)caseInsensitiveFinder : caseSensitiveFinder;
+      }
+
+      protected static IMaybe<int> caseSensitiveFinder(string item, List<string> list)
+      {
+         var index = list.IndexOf(item);
+         return maybe(index > -1, () => index);
+      }
+
+      protected static IMaybe<int> caseInsensitiveFinder(string item, List<string> list)
+      {
+         for (var i = 0; i < list.Count; i++)
+         {
+            if (list[i].Same(item))
+            {
+               return i.Some();
+            }
+         }
+
+         return none<int>();
+      }
+
       protected StringComparison stringComparison;
+      protected Func<string, List<string>, IMaybe<int>> finder;
 
       public StringSet() { }
 
-      public StringSet(bool ignoreCase) => stringComparison = getStringComparison(ignoreCase);
+      public StringSet(bool ignoreCase)
+      {
+         stringComparison = getStringComparison(ignoreCase);
+         finder = getFinder(ignoreCase);
+      }
 
       public StringSet(IEnumerable<string> strings) : base(strings) { }
 
       public StringSet(IEnumerable<string> strings, bool ignoreCase) : base(strings)
       {
          stringComparison = getStringComparison(ignoreCase);
+         finder = getFinder(ignoreCase);
       }
 
       public StringSet(params string[] items) : base(items) { }
@@ -32,7 +63,11 @@ namespace Core.Collections
       public bool IgnoreCase
       {
          get => stringComparison == StringComparison.OrdinalIgnoreCase;
-         set => stringComparison = getStringComparison(value);
+         set
+         {
+            stringComparison = getStringComparison(value);
+            finder = getFinder(value);
+         }
       }
 
       public override bool Contains(string item)
@@ -50,6 +85,14 @@ namespace Core.Collections
       public IMaybe<bool> IsExactlyEqualTo(string target)
       {
          return content.FirstOrNone(i => i.IsExactlyEqualTo(target).HasValue).Map(r => r.IsExactlyEqualTo(target));
+      }
+
+      public override void Remove(string item)
+      {
+         if (finder(item, content).If(out var index))
+         {
+            content.RemoveAt(index);
+         }
       }
    }
 }
