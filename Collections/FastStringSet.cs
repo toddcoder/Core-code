1250423766abb9160ed89af1428f689906417fa1
Todd Bennett <tebennett@eprod.com>
2021-04-07 13:33:25 -0500

    Added FastSet and FastStringSet

---
+++ Collections/FastStringSet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Collections
+{
+   public class FastStringSet : FastSet<string>
+   {
+      protected bool ignoreCase;
+
+      protected static IEqualityComparer<string> getEqualityComparer(bool ignoreCase)
+      {
+         return ignoreCase ? (IEqualityComparer<string>)new IgnoreCaseEqualityComparer() : EqualityComparer<string>.Default;
+      }
+
+      public FastStringSet(bool ignoreCase) : base(getEqualityComparer(ignoreCase))
+      {
+         this.ignoreCase = ignoreCase;
+      }
+
+      public FastStringSet(bool ignoreCase, IEnumerable<string> items) : base(getEqualityComparer(ignoreCase), items)
+      {
+         this.ignoreCase = ignoreCase;
+      }
+
+      public FastStringSet(bool ignoreCase, params string[] items) : base(getEqualityComparer(ignoreCase), items)
+      {
+         this.ignoreCase = ignoreCase;
+      }
+
+      public bool IgnoreCase => ignoreCase;
+   }
+}
