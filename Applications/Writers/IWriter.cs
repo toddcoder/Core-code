4477f12fbfd18ac05a972342000552a5dd9af974
Todd Bennett <tebennett@eprod.com>
2021-05-24 10:15:45 -0500

    Reformatted

--- Applications/Writers/IWriter.cs
+++ Applications/Writers/IWriter.cs
@@ -2,28 +2,28 @@
 
 namespace Core.Applications.Writers
 {
-	public interface IWriter
-	{
-		void WriteRaw(string text);
+   public interface IWriter
+   {
+      void WriteRaw(string text);
 
-		void Write(string message);
+      void Write(string message);
 
-		void Write(object message);
+      void Write(object message);
 
-		void WriteLine(string message);
+      void WriteLine(string message);
 
-		void WriteLine(object message);
+      void WriteLine(object message);
 
-		void WriteException(Exception exception);
+      void WriteException(Exception exception);
 
-		void WriteException(string message);
+      void WriteException(string message);
 
-		void WriteException(object message);
+      void WriteException(object message);
 
-		void WriteExceptionLine(Exception exception);
+      void WriteExceptionLine(Exception exception);
 
-		void WriteExceptionLine(string message);
+      void WriteExceptionLine(string message);
 
-		void WriteExceptionLine(object message);
-	}
+      void WriteExceptionLine(object message);
+   }
 }
