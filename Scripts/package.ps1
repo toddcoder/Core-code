f7c20c71096208790c95a49dd2a3fc49d10c1335
Todd Bennett <tebennett@eprod.com>
2021-07-08 10:36:25 -0500

    Forgot updated files

--- Scripts/package.ps1
+++ Scripts/package.ps1
@@ -2,6 +2,7 @@ $target = "..\Core.lib\lib\net45"
 Copy-Item "..\bin\Debug\Core.dll" $target
 Copy-Item "..\Core.Internet\bin\Debug\Core.Internet.dll" $target
 Copy-Item "..\Core.Data\bin\Debug\Core.Data.dll" $target
+Copy-Item "..\Core.WinForms\bin\Debug\Core.WinForms.dll" $target
 Push-Location "..\Core.lib"
 Remove-Item "*.nupkg"
 & 'C:\Program Files\dotnet\nuget.exe' pack .\Core.nuspec
