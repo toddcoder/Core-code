622381d8519f1c65d0ea62202927bce430d70a55
Todd Bennett <tebennett@eprod.com>
2021-07-30 11:21:30 -0500

    new Markup Assembly with Rtf

--- Scripts/package.ps1
+++ Scripts/package.ps1
@@ -3,6 +3,7 @@ Copy-Item "..\bin\Debug\Core.dll" $target
 Copy-Item "..\Core.Internet\bin\Debug\Core.Internet.dll" $target
 Copy-Item "..\Core.Data\bin\Debug\Core.Data.dll" $target
 Copy-Item "..\Core.WinForms\bin\Debug\Core.WinForms.dll" $target
+Copy-Item "..\Core.Markup\bin\Debug\Core.Markup.dll" $target
 Push-Location "..\Core.lib"
 Remove-Item "*.nupkg"
 & 'C:\Program Files\dotnet\nuget.exe' pack .\Core.nuspec
