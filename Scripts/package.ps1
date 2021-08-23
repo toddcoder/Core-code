9740bb8f0a0b393daa5b6167b270904288905b77
Todd Bennett <tebennett@eprod.com>
2021-08-23 13:09:12 -0500

    Fixed package to include Core.Git.dll

--- Scripts/package.ps1
+++ Scripts/package.ps1
@@ -5,6 +5,7 @@ Copy-Item "..\Core.Data\bin\Debug\Core.Data.dll" $target
 Copy-Item "..\Core.WinForms\bin\Debug\Core.WinForms.dll" $target
 Copy-Item "..\Core.Markup\bin\Debug\Core.Markup.dll" $target
 Copy-Item "..\Core.Json\bin\Debug\Core.Json.dll" $target
+Copy-Item "..\Core.Git\bin\Debug\Core.Git.dll" $target
 Push-Location "..\Core.lib"
 Remove-Item "*.nupkg"
 & 'C:\Program Files\dotnet\nuget.exe' pack .\Core.nuspec
