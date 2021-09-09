89c076493822a6a2b4bbeb8c65b80122ebbbfc86
Todd Bennett <toddcoder@comcast.net>
2021-09-09 08:03:23 -0500

    Started RichLabel

---
+++ Core.WinForms/Controls/RichLabel.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows.Forms;
+
+namespace Core.WinForms.Controls
+{
+   public partial class RichLabel : RichTextBox
+   {
+      protected const int WM_R_BUTTON_DOWN = 0x204;
+      protected const int WM_R_BUTTON_UP = 0x205;
+
+      public RichLabel()
+      {
+         InitializeComponent();
+
+         ReadOnly = true;
+         BorderStyle = BorderStyle.None;
+         TabStop = false;
+         SetStyle(ControlStyles.Selectable, false);
+         SetStyle(ControlStyles.UserMouse, true);
+         SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+
+         MouseEnter += (_, _) => Cursor = Cursors.Default;
+      }
+
+      protected override void WndProc(ref Message m)
+      {
+         if (m.Msg is 0x204 or 0x205)
+         {
+            return;
+         }
+
+         base.WndProc(ref m);
+      }
+
+      public void Display(string message)
+      {
+
+      }
+   }
+}
