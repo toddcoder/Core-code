e9ceae2a11d1f137c0946a2f1266f72f55514b18
Todd Bennett <tebennett@eprod.com>
2021-10-04 12:37:34 -0500

    Added IEnumerableHash interface

---
+++ Collections/IEnumerableHash.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Collections
+{
+   public interface IEnumerableHash<TKey, TValue> : IHash<TKey, TValue>
+   {
+      IEnumerable<(TKey, TValue)> Enumerable();
+   }
+}
