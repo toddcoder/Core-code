02efa58809a2e65f90e05885d2586a358f7b81ca
Todd Bennett <tebennett@eprod.com>
2022-03-16 12:49:43 -0500

    Added uninitialized message

--- Core.WinForms/LabelExtensions.cs
+++ Core.WinForms/LabelExtensions.cs
@@ -6,32 +6,23 @@ namespace Core.WinForms
 {
    public static class LabelExtensions
    {
-      public static void ShowMessage(this Label label, string message)
+      public static void ShowMessage(this Label label, string message, Color foreColor, Color backColor)
       {
-         label.ForeColor = Color.White;
-         label.BackColor = Color.Blue;
+         label.ForeColor = foreColor;
+         label.BackColor = backColor;
          label.Text = message;
       }
 
-      public static void ShowException(this Label label, Exception exception)
-      {
-         label.ForeColor = Color.White;
-         label.BackColor = Color.Red;
-         label.Text = exception.Message;
-      }
+      public static void ShowMessage(this Label label, string message) => label.ShowMessage(message, Color.White, Color.Blue);
 
-      public static void ShowSuccess(this Label label, string message)
-      {
-         label.ForeColor = Color.White;
-         label.BackColor = Color.Green;
-         label.Text = message;
-      }
+      public static void ShowException(this Label label, Exception exception) => label.ShowMessage(exception.Message, Color.White, Color.Red);
 
-      public static void ShowFailure(this Label label, string message)
-      {
-         label.ForeColor = Color.Black;
-         label.BackColor = Color.Yellow;
-         label.Text = message;
-      }
+      public static void ShowSuccess(this Label label, string message) => label.ShowMessage(message, Color.White, Color.Green);
+
+      public static void ShowFailure(this Label label, string message) => label.ShowMessage(message, Color.Black, Color.Yellow);
+
+      public static void ShowBusy(this Label label, string message) => label.ShowMessage(message, Color.Red, Color.Gold);
+
+      public static void ShowUninitialized(this Label label, string message) => label.ShowMessage(message, Color.DarkGray, Color.Gray);
    }
 }
