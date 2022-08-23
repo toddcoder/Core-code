5870b97c3dba0f3368cd0d3cdd9f5ad231a01a2f
Todd Bennett <TEBENNETT@eprod.com>
2022-08-23 07:14:13 -0500

    Removed unnecessary using

--- Core.Tests/WinFormsTests.cs
+++ Core.Tests/WinFormsTests.cs
@@ -4,7 +4,6 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Core.Dates.DateIncrements;
 using Core.Strings;
