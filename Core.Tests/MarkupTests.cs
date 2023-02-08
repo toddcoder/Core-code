cded26479789c56a4ffde63276250b866bc047e6
Todd Bennett <tebennett@eprod.com>
2023-02-08 09:23:28 -0600

    Added new html test'

--- Core.Tests/MarkupTests.cs
+++ Core.Tests/MarkupTests.cs
@@ -2,6 +2,7 @@
 using Core.Markup.Html;
 using Core.Markup.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using static Core.Arrays.ArrayFunctions;
 
 namespace Core.Tests;
 
@@ -82,6 +83,68 @@ public class MarkupTests
       tr *= "td>ah";
       tr *= "td>bo";
       _ = tr * "td>tso";
+
+      Console.WriteLine(builder);
+   }
+
+   [TestMethod]
+   public void HtmlTest2()
+   {
+      var builder = new HtmlBuilder();
+
+      var defaultStyle = builder + ".header{";
+      _ = defaultStyle + "color: white";
+      _ = defaultStyle + "background-color: blue";
+
+      var titleStyle = builder + ".title{";
+      _ = titleStyle + "font-weight: bold";
+      _ = titleStyle + "font-size: 16px";
+
+      var boldStyle = builder + ".bold{";
+      _ = boldStyle + "font-weight: bold";
+      _ = boldStyle + "font-size: 14px";
+
+      var body = builder.Body;
+      _ = body + "@style='font-family: Verdana; font-size: 11px'";
+
+      var p = body + "p>Merged Branches";
+      _ = p + "@class=title";
+
+      var table = body + "table>";
+      _ = table + "@border=1px black solid";
+      var th = table + "th>";
+      _ = th + "@class=header";
+      _ = th + "b>Branch";
+
+      var tr = table + "tr>";
+      _ = tr + "td>Alpha";
+      tr = table + "tr>";
+      _ = tr + "td>Bravo";
+      tr = table + "tr>";
+      _ = tr + "td>Charlie";
+
+      _ = body + "hr>";
+      p = body + "p>Conflicted Branches";
+      _ = p + "@class=title";
+
+      foreach (var branch in array("branch1", "branch2", "branch3"))
+      {
+         _ = body + "hr>";
+
+         table = body + "table>";
+         _ = table + "@border=1px black solid";
+         th = table + $"th>{branch}";
+         _ = th + "@class=header";
+         th = table + "th>File";
+         _ = th + "@class=header";
+
+         foreach (var file in array("file1", "file2", "file3", "file4"))
+         {
+            tr = table + "tr>";
+            _ = tr + "td>";
+            _ = tr + $"td>{file}";
+         }
+      }
 
       Console.WriteLine(builder);
    }
