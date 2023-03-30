bb17fcf7377da2f7df974b04631f9c45389a6738
Todd Bennett <tebennett@eprod.com>
2023-03-30 08:03:22 -0500

    Added locking

--- Applications/Messaging/MessageQueue.cs
+++ Applications/Messaging/MessageQueue.cs
@@ -9,27 +9,50 @@ public static class MessageQueue
 {
    private static AutoStringHash<MaybeQueue<Message>> queues;
    private static AutoStringHash<List<IMessageQueueListener>> listeners;
+   private static object locker;
 
    static MessageQueue()
    {
       queues = new AutoStringHash<MaybeQueue<Message>>(true, _ => new MaybeQueue<Message>(), true);
       listeners = new AutoStringHash<List<IMessageQueueListener>>(true, _ => new List<IMessageQueueListener>(), true);
+      locker = new object();
    }
 
    public static void Enqueue(string sender, Message message)
    {
-      queues[sender].Enqueue(message);
+      lock (locker)
+      {
+         queues[sender].Enqueue(message);
+      }
+
       foreach (var messageListener in listeners[sender])
       {
          messageListener.MessageFrom(sender);
+      }
+
+      lock (locker)
+      {
+         queues[sender].Dequeue();
       }
    }
 
    public static void Enqueue(string sender, string subject, object cargo) => Enqueue(sender, new Message(subject, cargo));
 
-   public static Maybe<Message> Dequeue(string sender) => queues.Maybe[sender].Map(q => q.Dequeue());
+   public static Maybe<Message> Dequeue(string sender)
+   {
+      lock (locker)
+      {
+         return queues.Maybe[sender].Map(q => q.Dequeue());
+      }
+   }
 
-   public static Maybe<Message> Peek(string sender) => queues.Maybe[sender].Map(q => q.Peek());
+   public static Maybe<Message> Peek(string sender)
+   {
+      lock (locker)
+      {
+         return queues.Maybe[sender].Map(q => q.Peek());
+      }
+   }
 
    public static void RegisterListener(string sender, IMessageQueueListener messageQueueListener) => listeners[sender].Add(messageQueueListener);
 
